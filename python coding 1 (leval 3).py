5f8d19298f09ba83c5e1aa7c11af8f009319a039
Jeyatheepan-Piranap <134526044+Jeyatheepan-Piranap@users.noreply.github.com>
2023-05-26 15:30:48 +0530

    Add files via upload

--- python coding 1 (leval 3).py
+++ python coding 1 (leval 3).py
@@ -1,3 +1,12 @@
 print('Hello, World!')
 person=input('What is your name?')
 print('Hello',person)
+
+age=input('What is your age?')
+print(age)
+weight=input('What is your Weight? (kg)')
+print(weight,'kg')
+height=input('What is your Height? (cm)')
+print(height,'cm')
+print('Thank you very much for the information') 
+
